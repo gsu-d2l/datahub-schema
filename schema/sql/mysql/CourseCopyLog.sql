0928731cf1e8b04582dc6cab554d6f3cecf0ad6e
Melody Forest <mforest@gsu.edu>
2024-03-19 08:19:02 -0400

    February 2024/20.24.2 updates

--- schema/sql/mysql/CourseCopyLog.sql
+++ schema/sql/mysql/CourseCopyLog.sql
@@ -10,6 +10,5 @@ CREATE TABLE `CourseCopyLog` (
   `IsDestinationNew` TINYINT DEFAULT NULL,
   `CopyProtectedResources` TINYINT DEFAULT NULL,
   `CopyCourseJobId` BIGINT NOT NULL,
-  `StatusId` INT DEFAULT NULL,
   UNIQUE KEY (`CopyCourseJobId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
